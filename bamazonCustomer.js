c10a9585e7385a8b157d505d2c5eac82aef6620a
Noah A <nanderson815@gmail.com>
2019-02-10 15:05:36 -0500

    added database connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,24 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 
+// Create connection to database.
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: "root",
+    password: "admin",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err){
+    if (err) throw err;
+});
+
+
+
+
+
+
 function buyProduct() {
     inquirer.prompt([
         {
