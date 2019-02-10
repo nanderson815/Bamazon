be3ef0bd88c9cf3ea4846770187c5f96b66d9eed
Noah A <nanderson815@gmail.com>
2019-02-10 14:36:49 -0500

    added products

--- bamazonDatabase.sql
+++ bamazonDatabase.sql
@@ -10,9 +10,23 @@ CREATE TABLE products
     item_id int identity(1,1) NOT NULL,
     product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(100) NOT NULL,
-    price INT (10) NOT NULL,
+    price DECIMAL (10,4) NOT NULL,
     stock_quantity INT (10) NOT NULL,
     PRIMARY KEY(item_id)
 );
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES
+("Samsung HDTV", "Electronics", 549.99, 22),
+("Roomba Smart Vacuum", "Electronics", 229.99, 4),
+("Computer Chair", "Furniture", 85.45, 10),
+("Down Comforter", "Homegoods", 299.99, 18),
+("Flatware Set", "Homegoods", 125.00, 35),
+("Dog Bed", "Pets", 89.99, 100),
+("Coffee Maker", "Homegoods", 115.99, 56),
+("Dining Room Table", "Furniture", 899.99, 15),
+("Shower Gel", "Toiletries", 8.99, 125),
+("Google Home Max", "Electronics", 99.99, 199),
+("Couch and Loveseat", "Furniture", 425.99, 7)
 
 
