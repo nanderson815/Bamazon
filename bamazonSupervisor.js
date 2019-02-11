403e3cf6111ad2f337a640469ef67e84d1e4a2b8
Noah A <nanderson815@gmail.com>
2019-02-10 21:24:18 -0500

    comleted print department info

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -49,17 +49,19 @@ function createDepartment() {
             type: 'input',
             message: 'Input the department monthly overhead:'
         }
-    ]).then(function(answer){
+    ]).then(function (answer) {
         connection.query(`INSERT INTO departments (department_name, over_head_costs) VALUES ('${answer.name}', ${answer.overhead})`);
         connection.end();
     })
-
 }
 
-function viewSales(){
-    connection.query(`SELECT * FROM departments`, function(err, resp){
-        printTable(resp);
-    })
+// Function to view the departments sales
+function viewSales() {
+    connection.query(`SELECT department_id, departments.department_name, departments.over_head_costs, SUM(product_sales) as product_sales, SUM(product_sales) - departments.over_head_costs as total_profit FROM departments JOIN products ON products.department_name = departments.department_name GROUP BY department_name ORDER BY department_id;`, 
+    function (err, resp) {
+            console.log(resp);
+            printTable(resp);
+        })
 }
 
 
@@ -71,6 +73,8 @@ function printTable(resp) {
         t.cell('Department ID', product.department_id)
         t.cell('Department Name', product.department_name)
         t.cell('Over Head Costs', product.over_head_costs)
+        t.cell('Department Sales', product.product_sales)
+        t.cell('Total Profit (Loss)', product.total_profit, Table.number(2))
         t.newRow()
     });
     console.log("\n" + t.toString());
